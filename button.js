f5a43f9764be03a7028926b8a493cded490f15bc
Revda <revda.1905@gmail.com>
2022-06-20 16:20:35 +0300

    selam canım ben amcanım

---
+++ button.js
@@ -0,0 +1,15 @@
+var status = "less";
+
+function toggleText()
+{
+    
+    if (status == "less") {
+        document.getElementById("textArea").innerHTML=text;
+        document.getElementById("toggleButton").innerText = "See Less";
+        status = "more";
+    } else if (status == "more") {
+        document.getElementById("textArea").innerHTML = "";
+        document.getElementById("toggleButton").innerText = "See More";
+        status = "less"
+    }
+}
